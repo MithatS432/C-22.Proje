1a63aec71d528a0c3899d74f4a68f9218bdeadf6
Mithat <mithatsirmali@gmail.com>
2025-04-25 16:42:12 +0300

    Derleyici Direktifleri ve Makrolar
    Basit Hoş Bir Proje

---
+++ Derleyici Direktifleri ve Makrolar2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+    int max = MAX(x, y);
+    printf("Maksimum deger: %d\n", max);
+
+    return 0;
+}
---
+++ Derleyici Direktifleri ve Makrolar2/math_utils.h
@@ -0,0 +1,7 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+// Makro ile maksimum deðeri bulma
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+#endif
